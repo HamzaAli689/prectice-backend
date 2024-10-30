30af575043a3b6009545e9521fde77be631c4b8e
HamzaAli689 <hamzaalivr33@gmail.com>
2024-10-30 21:11:05 +0500

    update dependences

--- test_second/lib/main.dart
+++ test_second/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'firebase_options.dart';
 import 'formpage/view.dart';
 
