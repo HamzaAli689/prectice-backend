f22298e047e7494ca84c0fabc45f13862c631e14
HamzaAli689 <hamzaalivr33@gmail.com>
2024-11-19 22:57:03 +0500

    update routes

--- fine_trackingapp/lib/Screens/sign_up_page/logic.dart
+++ fine_trackingapp/lib/Screens/sign_up_page/logic.dart
@@ -1,5 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fine_trackingapp/Screens/user_dashboard/view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../home__page/view.dart';
+
 class SignUp_pageLogic extends GetxController {
+
+  final nameCS = TextEditingController();
+  final emailCS = TextEditingController();
+  final passwordCS = TextEditingController();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final firestore = FirebaseFirestore.instance;
+
+  Future<void> CreateUseronFirebase() async {
+    // setState(() {
+    //   isLoading = true;  // Show loading spinner
+    // });
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: emailCS.text,
+        password: passwordCS.text,
+      );
+      await firestore.collection('users').doc(userCredential.user!.uid).set({
+        'name': nameCS.text,
+        'email': emailCS.text,
+        'uid': userCredential.user!.uid,
+      });
+      Get.to(Home_Page());  // Navigate to Dashboard after successful signup
+    } catch (e) {
+      Get.snackbar('Error', e.toString(), backgroundColor: Colors.red, colorText: Colors.white);
+    }
+  }
 
 }
--- fine_trackingapp/lib/Screens/sign_up_page/view.dart
+++ fine_trackingapp/lib/Screens/sign_up_page/view.dart
@@ -1,3 +1,6 @@
+import 'package:fine_trackingapp/Screens/login__page/view.dart';
+import 'package:fine_trackingapp/widgets/my_buttons.dart';
+import 'package:fine_trackingapp/widgets/mytext_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,8 +13,68 @@ class SignUp_page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey[800],
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black45,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        MytextFields(lable: "User Name", mycontroller: logic.nameCS, obscureText: false),
+                         SizedBox(height: 15),
+                        MytextFields(lable: "Email", mycontroller: logic.emailCS, obscureText: false),
+                        const SizedBox(height: 15),
+                        MytextFields(lable: "Password", mycontroller: logic.passwordCS, obscureText: true),
+                        const SizedBox(height: 20),
+                        MyButtons(onpress: (){
+                          logic.CreateUseronFirebase();
+                        }, title: "Sign UP"),
+                         SizedBox(height: 10),
+                        TextButton(
+                          onPressed: (){
+                            Get.to(Login_Page());
+                          },
+                          child: const Text(
+                            'Already hav an account? Sign In',
+                            style: TextStyle(color: Colors.cyanAccent),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
