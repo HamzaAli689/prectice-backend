1410fad6102b1fcb8b2745269f0cf30fbf8f7d64
HamzaAli689 <hamzaalivr33@gmail.com>
2024-11-20 21:10:30 +0500

    delete the hoe logic.dart

--- fine_trackingapp/lib/Screens/sign_up_page/logic.dart
+++ fine_trackingapp/lib/Screens/sign_up_page/logic.dart
@@ -1,37 +1,54 @@
+import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fine_trackingapp/Screens/user_dashboard/view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import '../../classes/users.dart';
 import '../home__page/view.dart';
 
 class SignUp_pageLogic extends GetxController {
-
   final nameCS = TextEditingController();
   final emailCS = TextEditingController();
   final passwordCS = TextEditingController();
   final FirebaseAuth auth = FirebaseAuth.instance;
   final firestore = FirebaseFirestore.instance;
 
-  Future<void> CreateUseronFirebase() async {
-    // setState(() {
-    //   isLoading = true;  // Show loading spinner
-    // });
+  Future<void> CreateUseronFirebase(String imageUrl) async {
     try {
       UserCredential userCredential = await auth.createUserWithEmailAndPassword(
         email: emailCS.text,
         password: passwordCS.text,
       );
-      await firestore.collection('users').doc(userCredential.user!.uid).set({
-        'name': nameCS.text,
-        'email': emailCS.text,
-        'uid': userCredential.user!.uid,
-      });
-      Get.to(Home_Page());  // Navigate to Dashboard after successful signup
+      Users myUser = Users(
+          name: nameCS.text,
+          email: emailCS.text,
+          password: passwordCS.text,
+          imageUrl: imageUrl);
+      await firestore.collection('users').doc(userCredential.user!.uid).set(
+        myUser.toJson()
+      );
+      Get.to(UserDashboardPage()); // Navigate to Dashboard after successful signup
     } catch (e) {
-      Get.snackbar('Error', e.toString(), backgroundColor: Colors.red, colorText: Colors.white);
+      Get.snackbar('Error', e.toString(),
+          backgroundColor: Colors.red, colorText: Colors.white);
+    }
+  }
+
+  Future<String?> imagePicker(Uint8List image, String folderPath) async {
+    String fileName = "Hamza.jpg";
+    try {
+      final Reference ref =
+      await FirebaseStorage.instance.ref().child(folderPath).child(fileName);
+      ref.putData(image);
+      final String myDownload = await ref.getDownloadURL();
+      print(myDownload);
+    } on Exception catch (e) {
+      print(e);
+      // TODO
     }
   }
 
 }
+
