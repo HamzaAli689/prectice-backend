c625d90c1ceb3461c44ef6581637c758ccc60aea
HamzaAli689 <hamzaalivr33@gmail.com>
2024-11-20 21:09:45 +0500

    update logic file

--- fine_trackingapp/lib/Screens/user_dashboard/logic.dart
+++ fine_trackingapp/lib/Screens/user_dashboard/logic.dart
@@ -1,5 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fine_trackingapp/classes/users.dart';
 import 'package:get/get.dart';
 
-class User_dashboardLogic extends GetxController {
+class UserDashboardLogic extends GetxController {
+  // RxList to make it observable
+  RxList<Users> userList = <Users>[].obs;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  // Fetch data from Firestore
+  Future<void> fetchData() async {
+    try {
+      QuerySnapshot allDocs = await firestore.collection("Users").get();
+      userList.clear(); // Clear any existing data to avoid duplication
+      for (var element in allDocs.docs) {
+        Users newUser = Users.fromJson(element.data() as Map<String, dynamic>);
+        userList.add(newUser);
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to fetch data: $e');
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchData(); // Fetch data when the controller is initialized
+  }
 }
