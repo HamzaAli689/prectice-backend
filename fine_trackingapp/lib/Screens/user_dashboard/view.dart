ab84016ef1e2ce9eb38eb1ef5e3e8b6e130f50b9
HamzaAli689 <hamzaalivr33@gmail.com>
2024-11-20 21:09:29 +0500

    update user_dashboard.dart file

--- fine_trackingapp/lib/Screens/user_dashboard/view.dart
+++ fine_trackingapp/lib/Screens/user_dashboard/view.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'logic.dart';
 
-class User_dashboardPage extends StatelessWidget {
-  User_dashboardPage({Key? key}) : super(key: key);
+class UserDashboardPage extends StatelessWidget {
+  UserDashboardPage({Key? key}) : super(key: key);
 
-  final User_dashboardLogic logic = Get.put(User_dashboardLogic());
+  final UserDashboardLogic logic = Get.put(UserDashboardLogic());
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        title: Text(
+          'Dashboard',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey[800],
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.admin_panel_settings,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // Uncomment and implement navigation to the admin dashboard screen
+              // Get.to(() => AdminDashboardScreen());
+            },
+          ),
+        ],
+      ),
+      body: Obx(() {
+        if (logic.userList.isEmpty) {
+          return Center(
+            child: CircularProgressIndicator(color: Colors.white),
+          );
+        }
+        return Expanded(
+          child: ListView.builder(
+            itemCount: logic.userList.length,
+            itemBuilder: (context, i) {
+              final data = logic.userList[i];
+              return Card(
+                color: Colors.blueGrey[800],
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(data.imageUrl),
+                    onBackgroundImageError: (_, __) => Icon(Icons.person),
+                  ),
+                  title: Text(
+                    data.name,
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  subtitle: Text(
+                    data.email,
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }
---
+++ fine_trackingapp/lib/classes/users.dart
@@ -0,0 +1,34 @@
+class Users {
+  String name;
+  String email;
+  String password;
+  String imageUrl;
+
+  // Constructor
+  Users({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.imageUrl,
+  });
+
+  // Convert a User object into a Map (toJson)
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  // Create a User object from a Map (fromJson)
+  factory Users.fromJson(Map<String, dynamic> json) {
+    return Users(
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      imageUrl: json['imageUrl'] as String,
+    );
+  }
+}
