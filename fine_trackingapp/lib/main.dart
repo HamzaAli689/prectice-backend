6bce09858c4cc7302fe251e50d55740fc1763d8d
HamzaAli689 <hamzaalivr33@gmail.com>
2024-11-25 16:13:18 +0500

    update main.dart file

--- fine_trackingapp/lib/main.dart
+++ fine_trackingapp/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Screens/sign_up_page/view.dart';
 import 'Screens/user_dashboard/view.dart';
+import 'abdullah/screens/home.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -24,25 +25,27 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fine Manager',
       theme: ThemeData.dark(),
-      initialRoute: '/Login_page',
-      getPages: [
-        GetPage(
-          name: '/SignUp_page',
-          page: () =>  SignUp_page(),
-        ),
-        GetPage(
-          name: '/Login_page',
-          page: () =>  Login_Page(),
-        ),
-        GetPage(
-          name: '/AdminPanelPage',
-          page: () =>  AdminPanelPage(),
-        ),
-        GetPage(
-          name: '/UserDashboardPage',
-          page: () => UserDashboardPage(),
-        ),
-      ],
+
+      // initialRoute: '/Login_page',
+      // getPages: [
+      //   GetPage(
+      //     name: '/SignUp_page',
+      //     page: () =>  SignUp_page(),
+      //   ),
+      //   GetPage(
+      //     name: '/Login_page',
+      //     page: () =>  Login_Page(),
+      //   ),
+      //   GetPage(
+      //     name: '/AdminPanelPage',
+      //     page: () =>  AdminPanelPage(),
+      //   ),
+      //   GetPage(
+      //     name: '/UserDashboardPage',
+      //     page: () => UserDashboardPage(),
+      //   ),
+      // ],
+      home: StudentApp(),
     );
   }
 }
