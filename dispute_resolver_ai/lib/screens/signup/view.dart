c311a195dddc676074649d1b0c4278238acaadf5
HamzaAli689 <hamzaalivr33@gmail.com>
2024-12-09 15:41:21 +0500

    create the folder of signup screen

---
+++ dispute_resolver_ai/lib/screens/signup/view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'logic.dart';
+
+class SignupPage extends StatelessWidget {
+  SignupPage({Key? key}) : super(key: key);
+
+  final SignupLogic logic = Get.put(SignupLogic());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
