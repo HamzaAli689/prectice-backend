c66e3f14969dc34a445e5d26dda8d31877b8caa7
HamzaAli689 <hamzaalivr33@gmail.com>
2024-12-10 09:16:52 +0500

    update the logic of homepage screen

--- dispute_resolver_ai/lib/screens/home/logic.dart
+++ dispute_resolver_ai/lib/screens/home/logic.dart
@@ -1,5 +1,12 @@
+import 'package:dispute_resolver_ai/models/users.dart';
 import 'package:get/get.dart';
 
 class HomeLogic extends GetxController {
+
+  List<MyUsers> myusers = [];
+  // Fetch all user fro firebase
+  GetUserFromFirebase() async {
+
+  }
 
 }
