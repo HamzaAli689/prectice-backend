f0606e3fac2303435ee31614ffbd9c9c70d18974
HamzaAli689 <hamzaalivr33@gmail.com>
2024-12-10 10:51:21 +0500

    update the home logic file

--- dispute_resolver_ai/lib/screens/home/logic.dart
+++ dispute_resolver_ai/lib/screens/home/logic.dart
@@ -1,12 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dispute_resolver_ai/models/users.dart';
 import 'package:get/get.dart';
 
 class HomeLogic extends GetxController {
+  List<MyUsers> myuserslist = [];
 
-  List<MyUsers> myusers = [];
   // Fetch all user fro firebase
   GetUserFromFirebase() async {
-
+    QuerySnapshot myCollection =
+        await FirebaseFirestore.instance.collection("Users").get();
+    for (var element in myCollection.docs) {
+      print(element.data());
+      MyUsers myuser = MyUsers.fromJson(element.data() as Map<String, dynamic>);
+      print(myuser.name);
+      myuserslist.add(myuser);
+    }
   }
 
+  @override
+  void onInit() async{
+    // TODO: implement onInit
+    super.onInit();
+    await GetUserFromFirebase();
+    myuserslist.forEach((e){
+      print(e.name);
+    });
+  }
 }
